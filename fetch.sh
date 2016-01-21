d461481b6477eb29a17fe24789775df306ece8bc
Old account ! My new one is @AntoineTurmel <antoineturmel@gmail.com>
2016-01-21 19:21:05 +0100

    updated script to find song/title on new RTL2 page

--- fetch.sh
+++ fetch.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
-# Retrieve RTL2 main page
-wget http://rtl2.fr
+date=`date +"%d/%m/%Y"`
+minute=`date +"%M"`
+hour=`date +"%H"`
+cd /var/www/data_rtl2
+# Retrieve RTL2 song info page
+wget http://www.rtl2.fr/quel-est-ce-titre --post-data 'date='"$date"'&hour='"$hour"'&minute='"$minute"
 # Look for song
-SONG=$(sed -n '/<p class="show">.*<\/p>/s/[^>]*>\([^<]*\)<.*/\1/p' index.html)
+SONG=$(sed -n '/<h2 class="song-title">.*<\/h2>/s/[^>]*>\([^<]*\)<.*/\1/p' quel-est-ce-titre | head -1)
 # and artist
-ARTIST=$(sed -n '/<p class="animateurs">.*<\/p>/s/[^>]*>\([^<]*\)<.*/\1/p' index.html)
+ARTIST=$(sed -n '/<p class="song-artist">.*<\/p>/s/[^>]*>\([^<]*\)<.*/\1/p' quel-est-ce-titre | head -1)
 # put both song and artist in text files
 echo $SONG > song.txt
 echo $ARTIST > artist.txt
-# remove index.html
-rm index.html*
+# remove temp page
+rm quel-est-ce-titre*
